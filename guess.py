fbcda02968109d6d720c46e55d70c06642184158
Gabriel Fonseca <gabrielfonseca91@hotmail.com>
2018-06-09 22:55:12 -0300

    Adding files

---
+++ guess.py
@@ -0,0 +1,23 @@
+import random
+def guess():
+    print('Take a guess: ')
+    number = int(input())
+    print('=' * 45)
+    return number
+
+def cpuNumber():
+    return random.randint(1, 20)
+
+count = 1
+cpuN = cpuNumber()
+print('I am thinking of a number between 1 and 20')
+while True:
+    n = guess()
+    if n < cpuN:
+        print('Your guess is too low!')
+    elif n > cpuN:
+        print('Your guess is too high!')
+    else:
+        print('Good job! You guessed my number in ' + str(count) + ' guesses.')
+        break
+    count += 1
